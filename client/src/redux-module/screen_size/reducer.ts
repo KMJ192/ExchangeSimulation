ddcb98bc47866e49d4f42b1271bc68364acf853b
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-02-10 22:51:52 +0900

    redux 로직

--- client/src/redux-module/screen_size/reducer.ts
+++ client/src/redux-module/screen_size/reducer.ts
@@ -2,15 +2,18 @@ import { RESIZE } from "./action";
 import { InitialScreenSize, ScreenSizeAction, ScreenSizeState } from "./types";
 
 //4. reducer 작성
-export default function screenReducer(state : ScreenSizeState = InitialScreenSize, action : ScreenSizeAction) {
-    switch(action.type){
-        case RESIZE:
-            return {
-                ...state,
-                width : action.screenWidth,
-                height : action.screenHeight
-            }
-        default:
-            return state;
-    }
+export default function screenReducer(
+  state: ScreenSizeState = InitialScreenSize,
+  action: ScreenSizeAction
+) {
+  switch (action.type) {
+    case RESIZE:
+      return {
+        ...state,
+        width: action.screenWidth,
+        height: action.screenHeight,
+      };
+    default:
+      return state;
+  }
 }
